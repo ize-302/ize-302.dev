6b33f4495bbeb1adbb505a91dc49c05899b53f28
ize-send <adavize.hassan@send.ng>
2023-02-27 06:10:53 +0100

    update projects

--- src/components/ProjectCard2.vue
+++ src/components/ProjectCard2.vue
@@ -1,8 +1,14 @@
 <template>
   <div class="p-4 rounded-md gap-24 text-gray-400 font-normal items-center">
-    <a :href="item.link.url" target="_blank">
+    <a v-if="!item.wip" :href="item.link.url" target="_blank">
       <img class="object-cover h-48 w-full rounded-md mb-6" :src="item.image" />
     </a>
+    <div
+      v-if="item.wip"
+      class="object-cover h-48 w-full rounded-md mb-6 bg-gray-900 text-3xl flex justify-center items-center"
+    >
+      🏗️
+    </div>
     <div class="">
       <p v-if="item.name" class="text-white font-normal text-sm mb-2">
         {{ item.name }}
@@ -15,7 +21,9 @@
         class="group mt-5 flex items-center gap-2 text-sm text-white"
         target="_blank"
         >{{ item.link.name }}
-        <span class="material-symbols-outlined text-sm group-hover:opacity-60"
+        <span
+          v-if="item.link.url"
+          class="material-symbols-outlined text-sm group-hover:opacity-60"
           >arrow_right_alt</span
         ></a
       >
